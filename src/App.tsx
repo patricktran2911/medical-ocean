8e2205a8fbc293e90558c4dc34fd0798d4857422
patricktran2911 <134541758+patricktran2911@users.noreply.github.com>
2024-02-29 18:51:25 -0800

    Change code on main
    change alignment for navbar to the left.
    change from const to function.

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ function BackgroundSetter({ children }: BackgroundSetterProps) {
     // Change the background based on the route
     switch (location.pathname) {
       case '/':
-        document.body.style.backgroundColor = '#000000'; // Example background color for home
+        document.body.style.backgroundColor = '#f0f0f0'; // Example background color for home
         break;
       case '/team-members':
         document.body.style.backgroundColor = '#b0c4de'; // Example background color for team-members
--- src/Components/NavBarComponent/NavBar.tsx
+++ src/Components/NavBarComponent/NavBar.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.css';
 
-const NavBar = () => {
+function NavBar() {
     return (
         <nav className='navbar'>
             <Link to="/">Home</Link>
             <Link to="/team-members">Team</Link>
         </nav>
-    );
-};
+    )
+}
 
 export default NavBar;
