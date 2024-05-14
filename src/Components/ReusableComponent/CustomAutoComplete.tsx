83ac1d958175326723a4ee9c0a75199797e2beed
patricktran2911 <134541758+patricktran2911@users.noreply.github.com>
2024-05-14 04:34:15 -0700

    fix bug

--- src/Components/ReusableComponent/CustomAutoComplete.tsx
+++ src/Components/ReusableComponent/CustomAutoComplete.tsx
@@ -4,131 +4,130 @@ import { useEffect, useState } from "react";
 import fuzzysearch from "fuzzysearch-ts";
 
 interface IOption {
-    id: string;
-    text: string;
+	id: string;
+	text: string;
 }
 
 interface IAutocomplete {
-    filtered?: IOption[];
-    selected?: IOption;
-    options: IOption[];
-    onSelected?: (option: IOption) => void;
+	filtered?: IOption[];
+	selected?: IOption;
+	options: IOption[];
+	onSelected?: (option: IOption) => void;
 }
 export default function AutoComplete({
-    options,
-    selected,
-    onSelected,
+	options,
+	selected,
+	onSelected,
 }: IAutocomplete) {
-    const [IData, setIData] = useState<IAutocomplete>({
-        filtered: [],
-        options: options,
-    });
+	const [IData, setIData] = useState<IAutocomplete>({
+		filtered: [],
+		options: options,
+	});
+	const [textValue, setTextValue] = useState("");
 
-    useEffect(() => {
-        setIData({
-            ...IData,
-            options: options,
-        });
-    }, [options]);
+	useEffect(() => {
+		setIData({
+			...IData,
+			options: options,
+			selected: selected,
+		});
+		setTextValue(selected?.text ?? "");
+	}, [options, selected]);
 
-    const [textValue, setTextValue] = useState("");
-    function handleTextChange(e: React.ChangeEvent<HTMLInputElement>) {
-        const value = e.target.value;
-        if (value.length === 0) {
-            setTextValue(e.target.value);
-            setIData({
-                ...IData,
-                filtered: [],
-            });
-        } else {
-            setTextValue(e.target.value);
-            setIData({
-                ...IData,
-                filtered: options.filter((option) =>
-                    fuzzysearch(e.target.value, option.text)
-                ),
-            });
-        }
-    }
+	function handleTextChange(e: React.ChangeEvent<HTMLInputElement>) {
+		const value = e.target.value;
+		if (value.length === 0) {
+			setTextValue(e.target.value);
+			setIData({
+				...IData,
+				filtered: [],
+			});
+		} else {
+			setTextValue(e.target.value);
+			setIData({
+				...IData,
+				filtered: options.filter((option) =>
+					fuzzysearch(e.target.value, option.text)
+				),
+			});
+		}
+	}
 
-    function handleSelectedOption(option: IOption) {
-        setTextValue(option.text);
-        setIData({
-            ...IData,
-            filtered: [],
-            selected: option,
-        });
-        onSelected?.(option);
-    }
+	function handleSelectedOption(option: IOption) {
+		setTextValue(option.text);
+		setIData({
+			...IData,
+			filtered: [],
+			selected: option,
+		});
+		onSelected?.(option);
+	}
 
-    return (
-        <Box
-            sx={(theme) => ({
-                width: "100%",
-                border: "solid",
-                borderColor: theme.palette.divider,
-                borderWidth: "3px",
-                borderRadius: "8px",
-                position: "relative",
-            })}
-        >
-            <TextField
-                onChange={handleTextChange}
-                sx={{ width: "100%" }}
-                value={textValue}
-            />
-            {IData.filtered && IData.filtered.length !== 0 && (
-                <List
-                    sx={(theme) => ({
-                        position: "absolute", // Position the list absolutely
-                        top: "100%", // Start the list right below the TextField
-                        left: 0,
-                        width: "100%",
-                        background: theme.palette.background.default,
-                        zIndex: "99999",
-                        maxHeight: "300px", // Set a max-height and make it scrollable
-                        border: "solid",
+	return (
+		<Box
+			sx={(theme) => ({
+				width: "100%",
+				border: "solid",
+				borderColor: theme.palette.divider,
+				borderWidth: "3px",
+				borderRadius: "8px",
+				position: "relative",
+			})}>
+			<TextField
+				onChange={handleTextChange}
+				sx={{ width: "100%" }}
+				defaultValue={selected?.text}
+				value={textValue}
+			/>
+			{IData.filtered && IData.filtered.length !== 0 && (
+				<List
+					sx={(theme) => ({
+						position: "absolute", // Position the list absolutely
+						top: "100%", // Start the list right below the TextField
+						left: 0,
+						width: "100%",
+						background: theme.palette.background.default,
+						zIndex: "99999",
+						maxHeight: "300px", // Set a max-height and make it scrollable
+						border: "solid",
 
-                        overflow: "auto",
-                        borderWidth: "3px",
-                        borderTopWidth: "0px",
-                        borderColor: theme.palette.divider,
-                    })}
-                >
-                    {IData.filtered.map((value) => (
-                        <ListItem
-                            key={value.id}
-                            id={value.id}
-                            sx={{
-                                width: "100%",
-                            }}
-                        >
-                            <Stack
-                                direction={"column"}
-                                spacing={"2px"}
-                                sx={{
-                                    width: "100%",
-                                }}
-                            >
-                                <ReusableButton
-                                    variant="text"
-                                    color="inherit"
-                                    onClick={() => {
-                                        handleSelectedOption(value);
-                                    }}
-                                    sx={{
-                                        width: "100%",
-                                        textTransform: "none",
-                                        justifyContent: "flex-start",
-                                    }}
-                                    text={value.text}
-                                />
-                                <Divider />
-                            </Stack>
-                        </ListItem>
-                    ))}
-                </List>
-            )}
-        </Box>
-    );
+						overflow: "auto",
+						borderWidth: "3px",
+						borderTopWidth: "0px",
+						borderColor: theme.palette.divider,
+					})}>
+					{IData.filtered.map((value) => (
+						<ListItem
+							key={value.id}
+							id={value.id}
+							sx={{
+								width: "100%",
+							}}>
+							<Stack
+								direction={"column"}
+								spacing={"2px"}
+								sx={{
+									width: "100%",
+								}}>
+								<ReusableButton
+									variant="text"
+									color="inherit"
+									onClick={() => {
+										handleSelectedOption(value);
+									}}
+									sx={{
+										width: "100%",
+										textTransform: "none",
+										justifyContent: "flex-start",
+									}}
+									text={value.text}
+								/>
+								<Divider />
+							</Stack>
+						</ListItem>
+					))}
+				</List>
+			)}
+		</Box>
+	);
 }
